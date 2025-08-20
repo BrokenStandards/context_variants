6b9c9a4f1a7d9c378c1b0520ab359d1167a96185
BrokenStandards <broken-commit@pub.todayi.dev>
2025-08-20 14:09:09 -0500

    Rename trybuild folder

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -1,6 +1,6 @@
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/fail_*.rs");
-    t.pass("tests/ui/pass_*.rs");
+    t.compile_fail("tests/tb/fail_*.rs");
+    t.pass("tests/tb/pass_*.rs");
 }
